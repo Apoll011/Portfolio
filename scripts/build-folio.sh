c3a5367429274657d5a9034ad3c38fee437bc8bf
Apoll0_11 <tiagorobotik@gmail.com>
2025-11-22 17:03:25 +0000

    Switched FolioSpace build script from npm to pnpm for dependency management and build process.

--- scripts/build-folio.sh
+++ scripts/build-folio.sh
@@ -1,6 +1,6 @@
 cd FolioSpace
-npm install
-npm run build
+pnpm install
+pnpm run build
 rm -rf ../developer-portfolio/public/foliospace
 mkdir -p ../developer-portfolio/public/foliospace
 cp -r dist/* ../developer-portfolio/public/foliospace

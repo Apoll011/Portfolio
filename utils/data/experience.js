4e93337f6371c22b499a950ab8c1425312fe1a40
Apoll0_11 <tiagorobotik@gmail.com>
2025-11-22 22:04:48 +0000

    Updated `experience.js` to include new professional experience details and adjusted existing entries; updated `index.jsx` to render experience descriptions.

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,8 +1,15 @@
 export const experiences = [
   {
     id: 1,
-    title: "Desempregado",
+    title: "Sem Atividade Profissional",
     company: "Programando e construindo algo todos os dias.",
-    duration: "(Jan 2023 - Presente)"
+    duration: "(Jan 2023 - Jul 2025)"
+  },
+  {
+    id: 2,
+    title: "Estagio Profissional",
+    company: "DoutVision Lda.",
+    duration: "(Jul 2025 - Nov 2025)",
+    description: "Fiz um estágo remoto sob a supervisáo da minha escola Secundária em que nesse, passei por todas as fazes de desenvolvimento de um aplicativo, desde a conceção da ideia até a publicação."
   }
 ]
